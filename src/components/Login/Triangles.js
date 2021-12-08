7c2442cc55a7ac2d9a6fb045cc47b398a1e57e10
Alenygam <alessandro@alenygam.com>
2021-12-08 21:02:25 +0100

    Handling diverso dei triangoli a lato, in modo da poterli animare

---
+++ src/components/Login/Triangles.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Triangles({loading}) {
+    return (
+        <div style={{overflow: 'hidden'}}>
+            <Triangle2 loading={loading}/>
+            <Triangle1 loading={loading}/>
+        </div>
+    );
+}
+
+const Triangle1 = styled.div`
+    background: #B84A62;
+    clip-path: polygon(0 0, 0 100%, calc(100% - 500px) 100%, 100% 0);
+    transform: ${props => props.loading 
+        ? "translateX(-50px)"
+        : "translateX(calc(-100% + 500px))"
+    };
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    
+    transition: 1s;
+`;
+const Triangle2 = styled.div`
+    background: #D98324;
+    clip-path: polygon(0 0, 0 100%, calc(100% - 500px) 100%, 100% 0);
+    transform: ${props => props.loading 
+        ? "translateX(-50px)"
+        : "translateX(calc(-100% + 550px))"
+    };
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: calc(100% + 50px);
+    position: absolute;
+
+    transition: 1s;
+`;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,32 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import Triangles from '../components/Login/Triangles';
 
 const Container = styled.div`
     background-color: #0A2239;
     color: #FFFFFF;
-    display: grid;
-    grid-template-columns: 500px 1fr;
-    grid-template-rows: 100%;
     height: 100%;
-`
-
-const LeftTriangle = styled.div`
     position: relative;
-    background: linear-gradient(
-        to bottom right, 
-        #D98324 0%, 
-        #D98324 50%, 
-        transparent 50%, 
-        transparent 100%
-    );
-`
-
+`;
 export default function Login() {
+    const [loading, setLoading] = React.useState(true);
     return (
         <Container>
-            <LeftTriangle>
-            </LeftTriangle>
-            <div></div>
+            <Triangles loading={loading}/>
         </Container>
-    )
+    );
 }
