7833fbf0430344c409d0c792e34ce4902187a3da
Alenygam <alessandro@alenygam.com>
2021-12-19 16:41:19 +0100

    Added Grades page

---
+++ src/pages/Grades.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components';
+import Spinner from 'react-spinkit';
+import {useNavigate} from 'react-router-dom';
+
+import Sidebar from '../parts/Sidebar';
+
+import Api from '../api/api';
+
+const Container = styled.div`
+    background-color: #0A2239;
+    color: #FFFFFF;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+`;
+
+
+export default function Grades() {
+    const [isAuthed, setIsAuthed] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const api = new Api();
+        if (api.token) return setIsAuthed(true);
+
+        navigate('/login');
+    }, [navigate])
+
+    if (!isAuthed) {
+        return (
+            <Container>
+                <div style={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'scale(1.5) translate(-50%, -50%)',
+                }}>
+                    <Spinner name="wandering-cubes" color="#D98324"/>
+                </div>
+            </Container>
+        )
+    }
+
+    return (
+        <Container>
+            <Sidebar/>
+        </Container>
+    )
+}
