01e84c31a8c3d6c4e83ac21094704da07f969216
Alenygam <alessandro@alenygam.com>
2021-12-24 23:46:48 +0100

    Modified slider

--- src/parts/Sliders/RoundReadOnlySlider.js
+++ src/parts/Sliders/RoundReadOnlySlider.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import { CircleSlider } from 'react-circle-slider';
+import React, {useState, useRef, useEffect} from 'react';
 
 export default function RoundReadOnlySlider({value, progressColor, size, progressWidth}) {
+    const center = size / 2;
+    const radius = size / 2 - progressWidth / 2;
+    const circumference = 2 * Math.PI * radius;
+    const actualValue = value * 10;
+
+    const circleRef = useRef();
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        const progressOffset = ((100 - actualValue) / 100) * circumference;
+        setOffset(progressOffset);
+    }, [circumference, actualValue])
     return (
-        <CircleSlider 
-            value={value} 
-            max={10} 
-            size={size} 
-            disabled={true} 
-            knobColor="transparent"
-            progressWidth={progressWidth}
-            progressColor={progressColor}
-            knobRadius={0}
-        />
+        <svg width={size} height={size} style={{transform: 'rotate(-90deg)'}}>
+            <circle
+                ref={circleRef}
+                stroke={progressColor}
+                fill="none"
+                cx={center}
+                cy={center}
+                r={radius}
+                strokeWidth={progressWidth}
+                strokeDasharray={circumference}
+                strokeDashoffset={offset}
+            />
+        </svg>
+        // <CircleSlider 
+        //     value={value} 
+        //     max={10} 
+        //     size={size} 
+        //     disabled={true} 
+        //     knobColor="transparent"
+        //     progressWidth={progressWidth}
+        //     progressColor={progressColor}
+        //     knobRadius={0}
+        // />
     )
 }
